d656a03afcdec772e7999e14b4ced22cc40d1c5e
Brian Smith <briaguya@gmail.com>
2016-12-29 17:08:45 -0500

    little cleanup

--- game.py
+++ game.py
@@ -9,9 +9,14 @@ class Game(object):
         self.deck = Deck()
         self.players = [Player(8 if numOfPlayers == 1 else 7 if numOfPlayers == 2 else 6) for p in range(numOfPlayers)]
 
-    def play(self):
-        self.deck.shuffle()
-        self.deck.deal([player.hand for player in self.players])
+    def show(self, player):
         print self.board
-        for player in self.players:
-            print player.hand
+        print player.hand
+
+    def setup(self):
+        self.deck.shuffle()
+        self.deck.deal([player.hand for player in self.players])        
+
+    def play(self):
+        self.setup()
+        for player in self.players: self.show(player)
