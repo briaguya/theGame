c6bdc4cc0c600cad0933b85d4a2fb9929270110b
Brian Smith <briaguya@gmail.com>
2017-01-03 22:33:34 -0500

    added another test

--- test_computerPlayer.py
+++ test_computerPlayer.py
@@ -12,8 +12,26 @@ class TestComputerPlayer(TestCase):
         player.hand.cards = [Card(n) for n in [90,85,95]]
         move = player.getMove(self.board,2)
 
+        # It should play all 3 cards
+        self.assertTrue(len(move.plays) == 3)
+
         # The last card in the move should be 95
         self.assertEqual(move.plays[-1][0],Card(95))
 
         # They should all play on the same pile
         self.assertTrue(all(play[1] == move.plays[-1][1] for play in move.plays))
+
+    def test_play_2_10_12(self):
+        player = ComputerPlayer(3,1)
+        player.hand.cards = [Card(n) for n in [2,10,12]]
+        move = player.getMove(self.board,2)
+
+        # It should play all 3 cards
+        self.assertTrue(len(move.plays) == 3)
+
+        # The last card in the move should be 2
+        self.assertEqual(move.plays[-1][0],Card(2))
+
+        # They should all play on the same pile
+        self.assertTrue(all(play[1] == move.plays[-1][1] for play in move.plays))
+
